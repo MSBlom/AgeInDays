b2229593c8b4ffcffabbd7f084c247b70b09505f
MSBlom <msblom2391@hotmail.com>
2022-04-13 09:48:52 +0200

    Versie 1

---
+++ Project 1/counting_days.h
@@ -0,0 +1,102 @@
+#pragma once
+
+namespace age_in_days {
+
+	bool schrikkeljaar(int year) {
+		// schikkeljaar als deelbaar door 4,
+		// behalve als deelbaar door 100,
+		// tenzij deelbaar door 400 (dus dan weer wel)
+		if (year % 100 == 0) {
+			int remainder = year % 400;
+			return (year % 400 == 0);
+		}
+		return year % 4 == 0;
+	}
+
+	int schikkeljaren_tot(int end_year) {
+		int result = 0;
+		for (int year = 1800; year < end_year; year++) {
+			if (schrikkeljaar(year)) {
+				result++;
+			}
+		}
+		return result;
+	}
+
+	struct Days_Result {
+		bool valid;
+		int days;
+	};
+
+	/// <summary>
+	/// Returns the number of days in that month.
+	/// </summary>
+	/// <param name="year">The year</param>
+	/// <param name="month">Month, indexed from 0 (jan.) to 11 (dec.)</param>
+	/// <returns>Days_Result</returns>
+	int days_in_month(int year, int month) {
+
+		if (month < 0 || month > 11) {
+			// sentinal value:
+			// return -1;
+
+			// flag in struct:
+			//return {false,0};
+
+			//throw std::runtime_error("")
+		}
+		const int months[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+		int schikkeldag = month == 1 && schrikkeljaar(year) ? 1 : 0;
+		int result = months[month] + schikkeldag;
+		return { true, result };
+	}
+
+
+	int kengetal_van(int year, int month, int day) {
+		// voorgaande jaren excl. schikkeljaar * 365
+		int days_before_this_year = (year - 1800) * 365;
+		// voorgaande schikkeljaren
+		int voorgaande_jaren = schikkeljaren_tot(year);
+		// voorgaande_maanden
+		int voorgaande_maanden = 0;
+		for (int i = 0; i < (month - 1); i++) {
+			voorgaande_maanden += days_in_month(year, i);
+		}
+		// dag optellen
+		return days_before_this_year + voorgaande_jaren + voorgaande_maanden + day;
+	}
+
+
+	struct Datum {
+		int year;
+		int month;
+		int day;
+
+		bool operator<(Datum rhs) {
+			if (rhs.year > year)
+				return true;
+
+			if (rhs.year < year)
+				return false;
+
+			if (rhs.month > month)
+				return true;
+
+			if (rhs.month < month)
+				return false;
+
+			return rhs.day > day;
+		}
+
+		bool operator>(Datum rhs) {
+			return *this < rhs;
+		}
+	};
+
+	int difference_days(Datum geboorte_datum, Datum vandaag) {
+		int kengetal_birth = kengetal_van(geboorte_datum.year, geboorte_datum.month, geboorte_datum.day);
+		int kengetal_today = kengetal_van(vandaag.year, vandaag.month, vandaag.day);
+		return kengetal_today - kengetal_birth;
+	}
+
+};
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,99 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../Project 1/counting_days.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(SchikkelJaren)
+		{
+			Assert::AreEqual(false, schrikkeljaar(1900));
+			Assert::AreEqual(false, schrikkeljaar(1877));
+			Assert::AreEqual(false, schrikkeljaar(2017));
+			Assert::AreEqual(false, schrikkeljaar(2021));
+
+			Assert::AreEqual(true, schrikkeljaar(2400), L"deelbaar door 400 moet wel schikkeljaar zijn");
+			Assert::AreEqual(true, schrikkeljaar(2000), L"deelbaar door 400 moet wel schikkeljaar zijn");
+
+			Assert::AreEqual(true, schrikkeljaar(4));
+			Assert::AreEqual(true, schrikkeljaar(1884));
+			Assert::AreEqual(true, schrikkeljaar(1204), L"deelbaar door 4 moet schikkeljaar zijn");
+		}
+
+		TEST_METHOD(Voorgaande_Maanden)
+		{
+			int actual = kengetal_van(1800, 3, 3);
+			Assert::AreEqual(62, actual, L"3 maart");
+		}
+
+		TEST_METHOD(Schrikkeljaar_proberen) 
+		{
+			int test1 = kengetal_van(2020, 3, 1);
+			int test2 = kengetal_van(2020, 2, 25);
+			int result = test1 - test2;
+			Assert::AreEqual(5, result, L"schrikkeljaar, dus 26 27 28 29 1");
+		}
+
+		TEST_METHOD(Geen_Schrikkeljaar_proberen)
+		{
+			int test1 = kengetal_van(2021, 3, 1);
+			int test2 = kengetal_van(2021, 2, 25);
+			int result = test1 - test2;
+			Assert::AreEqual(4, result, L"geen schrikkeljaar, dus 26 27 28 1");
+		}
+		TEST_METHOD(schikkeljaren_test)
+		{
+			int aantal_schikkeljaren = schikkeljaren_tot(1800);
+			Assert::AreEqual(0, aantal_schikkeljaren, L"refferentie jaar is 1800");
+
+			int aantal_schikkeljaren2 = schikkeljaren_tot(1805);
+			Assert::AreEqual(1, aantal_schikkeljaren2, L"1 schikkeljaar is 1805");
+		}
+
+		TEST_METHOD(een_vol_jaar_van_365_dagen)
+		{
+			int datum_nu = kengetal_van(2022, 4, 22);
+			int datum_geboorte = kengetal_van(2021, 4, 22);
+			int result = datum_nu - datum_geboorte;
+			Assert::AreEqual(365, result, L"een vol jaar");
+		}
+		TEST_METHOD(correct_date) 
+		{
+			Datum date_today{ 2022, 4, 11 };
+			Datum date_birthday{ 2022, 5, 10 };
+			bool result = date_today < date_birthday;
+			Assert::AreEqual(true, result, L"It's in the future.");
+		}
+
+		TEST_METHOD(future_not_before_now)
+		{
+			Datum future{ 2401, 1, 1 };
+			Datum now{ 2022, 5, 10 };
+			Assert::IsFalse(future < now);
+		}
+
+		TEST_METHOD(same_date_not_before_each_other)
+		{
+			Datum t1{ 2022, 5, 10 };
+			Assert::IsFalse(t1 < t1);
+			Assert::IsFalse(t1 > t1);
+		}
+
+		TEST_METHOD(invalid_days_in_month_call)
+		{
+			Days_Result days = days_in_month(2022, 33);
+			if (days.valid) {
+				// goed, verder
+			}
+			else {
+				cout << "Dat gaat niet." << endl;
+			}
+		}
+	};
+}
